5a1c3f80c26e61400e8723220b8cc317c4d78e76
Ekaterina Schedlberger <ek.schedl@gmail.com>
2025-04-02 14:31:59 +0200

    Buchstaben gemacht

---
+++ src/at/campus02/nowa/figures/FigureI.java
@@ -0,0 +1,35 @@
+package at.campus02.nowa.figures;
+
+public class FigureI extends Figure {
+
+    //    public FigureI() {
+//
+//        symbol[0][0] = ' ';
+//        symbol[1][0] = ' ';
+//        symbol[2][0] = ' ';
+//
+//
+//        symbol[0][1] = 'X';
+//        symbol[1][1] = 'X';
+//        symbol[2][1] = 'X';
+//
+//        symbol[0][2] = ' ';
+//        symbol[1][2] = ' ';
+//        symbol[2][2] = ' ';
+//
+//    }
+    //meine
+    public FigureI() {
+        symbol[0][0] = 'X';
+        symbol[1][0] = ' ';
+        symbol[2][0] = ' ';
+
+        symbol[0][1] = 'X';
+        symbol[1][1] = ' ';
+        symbol[2][1] = ' ';
+
+        symbol[0][2] = 'X';
+        symbol[1][2] = ' ';
+        symbol[2][2] = ' ';
+    }
+}
---
+++ src/at/campus02/nowa/figures/FigureL.java
@@ -0,0 +1,35 @@
+package at.campus02.nowa.figures;
+
+public class FigureL extends Figure {
+
+//    public FigureL() {
+//
+//        symbol[0][0] = 'X';
+//        symbol[1][0] = 'X';
+//        symbol[2][0] = 'X';
+//
+//
+//        symbol[0][1] = ' ';
+//        symbol[1][1] = ' ';
+//        symbol[2][1] = 'X';
+//
+//        symbol[0][2] = ' ';
+//        symbol[1][2] = ' ';
+//        symbol[2][2] = 'X';
+//
+//    }
+//meine
+public FigureL() {
+    symbol[0][0] = 'X';
+    symbol[1][0] = ' ';
+    symbol[2][0] = ' ';
+
+    symbol[0][1] = 'X';
+    symbol[1][1] = ' ';
+    symbol[2][1] = ' ';
+
+    symbol[0][2] = 'X';
+    symbol[1][2] = 'X';
+    symbol[2][2] = 'X';
+}
+}
